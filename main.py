ca615b5d14e9dec975c715e0bf52112e2bd3b46d
Anubhab Sen <anubhabsen@gmail.com>
2017-03-19 22:20:10 +0530

    shortlist for files in an interval

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import os
 import mimetypes
 import sys
@@ -14,7 +15,7 @@ def format_data(table):
         for word in row:
             if max < len(word):
                 max = len(word)
-    col = max
+    col = max + 3
     for row in table:
         print("".join(word.ljust(col) for word in row))
 
@@ -24,8 +25,14 @@ def get_type(path):
 def list_files(flag, argv):
     table = [['Title', 'Size', 'TimeStamp', 'Type']]
     for file in shared_files:
+        time = os.path.getmtime(os.path.join(curr_path, file))
         path = os.path.join(curr_path, file)
-        table.append([file, str(os.path.getsize(path)), str(os.path.getmtime(path)), get_type(path)])
+        if flag == 'shortlist':
+            start_time = float(argv[0])
+            end_time = float(argv[1])
+            if time < start_time or time > end_time:
+                continue
+        table.append([file, str(os.path.getsize(path)), str(time), get_type(path)])
     format_data(table)
 
 if __name__ == '__main__':
