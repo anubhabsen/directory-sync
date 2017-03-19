199bafb07175dd3644f034bde8569c6ee91e8297
Anubhab Sen <anubhabsen@gmail.com>
2017-03-19 21:58:15 +0530

    lists files

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ import sys
 curr_path = '.'
 shared_files = []
 for file in os.listdir(curr_path):
-	if os.path.isfile(os.path.join(curr_path, file)):
-		shared_files.append(file)
+    if os.path.isfile(os.path.join(curr_path, file)):
+        shared_files.append(file)
+
+def format_data(table):
+    max = 0
+    for row in table:
+        for word in row:
+            if max < len(word):
+                max = len(word)
+    col = max
+    for row in table:
+        print("".join(word.ljust(col) for word in row))
 
 def get_type(path):
-	return mimetypes.guess_type(path)[0]
+    return mimetypes.guess_type(path)[0] or 'text/plain'
 
 def list_files(flag, argv):
-	print('yes')
-	print('title\tsize\ttimestamp\ttype')
-	for file in shared_files:
-		path = os.path.join(curr_path, file)
-		print(file + '\t' + str(os.path.getsize(path)) + '\t' + str(os.path.getmtime(path)) + '\t' + get_type(path))
+    table = [['Title', 'Size', 'TimeStamp', 'Type']]
+    for file in shared_files:
+        path = os.path.join(curr_path, file)
+        table.append([file, str(os.path.getsize(path)), str(os.path.getmtime(path)), get_type(path)])
+    format_data(table)
 
 if __name__ == '__main__':
     if sys.argv[1] == 'index':
