ab003584ea06cbb2d1a7974e4cf84c3e3dc6220c
Anubhab Sen <anubhabsen@gmail.com>
2017-03-20 18:28:09 +0530

    remove comments

--- main.py
+++ main.py
@@ -12,52 +12,6 @@ for file in os.listdir(curr_path):
     if os.path.isfile(os.path.join(curr_path, file)):
         shared_files.append(file)
 
-############## Files list
-
-# def format_data(table):
-#     max = 0
-#     for row in table:
-#         for word in row:
-#             if max < len(word):
-#                 max = len(word)
-#     col = max + 3
-#     for row in table:
-#         print("".join(word.ljust(col) for word in row))
-
-# def get_type(path):
-#     return mimetypes.guess_type(path)[0] or 'text/plain'
-
-# def list_files(flag, argv):
-#     table = [['Title', 'Size', 'TimeStamp', 'Type']]
-#     for file in shared_files:
-#         time = os.path.getmtime(os.path.join(curr_path, file))
-#         path = os.path.join(curr_path, file)
-#         if flag == 'shortlist':
-#             start_time = float(argv[0])
-#             end_time = float(argv[1])
-#             if time < start_time or time > end_time:
-#                 continue
-#         elif flag == 'regex':
-#             if not re.fullmatch('.*' + argv[0], file):
-#                 continue
-#         table.append([file, str(os.path.getsize(path)), str(time), get_type(path)])
-#     format_data(table)
-
-############## Hashing
-
-# def get_hash(path):
-#     with open(path, 'rb') as file_hash:
-#         hash_val = hashlib.md5()
-#         while True:
-#             data = file_hash.read(4096)
-#             if not data:
-#                 break
-#             hash_val.update(data)
-#         return hash_val.hexdigest()
-
-# def change_details(path):
-#     return(get_hash(path), os.path.getmtime(path))
-
 ############## Download
 
 def download_file(name, sock):
